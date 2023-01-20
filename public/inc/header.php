df8c9352a9cf555828283eb3cd7ebed50ab7db8b
LexfStudio <lexfstudio@gmail.com>
2023-01-20 15:26:06 -0300

    aula06

---
+++ public/inc/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="<?php echo PATH_CSS.'reset.css' ?>">
+    <link rel="stylesheet" href="<?php echo PATH_CSS.'style.css' ?>">
+    <link rel="stylesheet" href="<?php echo PATH_CSS.'media.css' ?>">
+    <title><?php echo $title ?></title>
+    <script src="<?php echo PATH_JS.'nav.js' ?>" defer></script>
+</head> 
+<body>
+
+<!-- Cabeçalho do site: Begin -->
+<header>
+    <div id="logo">
+        <a href="<?php echo PATH_INDEX ?>">
+            <img src="<?php echo PATH_IMG.'logo.png' ?>" alt="Lexf Studio">
+        </a>
+    </div>
+    <!-- Menu principal: Begin -->
+    <nav id="nav-main-body">
+        <div id="nav-main-grp-desktop">
+            <button><a href="<?php echo MENU_HTML5 ?>">HTML5</a></button>
+            <button><a href="#">CSS3</a></button>
+            <button><a href="#">JavaScript</a></button>
+            <button><a href="#">PHP</a></button>
+            <button><a href="#">MySQL</a></button>
+        </div>
+        <button id="nav-main-btn-mobile"><a href="#"><span class="nav-main-span-dashes"></span></a></button>
+    </nav>
+    <!-- Menu principal: End -->
+</header>
+<!-- Cabeçalho do site: End -->
